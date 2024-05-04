2df1dbd9be9928cf70d2d20915e11e7bd3542e5d
hossein <hossein.h.a770227@gmail.com>
2024-05-04 19:11:12 +0330

    add Information model

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Information(models.Model):
+    CONDITION_CHOICES = (
+        ('r', _('ready')),
+        ('un', _("unprepared")),
+    )
+    fist_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    age = models.PositiveIntegerField()
+    city = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    phone = models.CharField(max_length=11)
+    condition = models.CharField(choices=CONDITION_CHOICES, max_length=50)
+    about_me = models.TextField()
+
